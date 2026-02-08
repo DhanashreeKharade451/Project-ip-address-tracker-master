bf92c921a47149323ee1630fa50a8be3bbf6da46
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-08 15:42:26 -0500

    saved data to local storage

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -13,14 +13,21 @@ const ispSpan = document.getElementById("isp");
 
 let initialized = false;
 
+// window.onload = () => {
+//     const savedTasks = localStorage.getItem("data");
+//     if (savedTasks) {
+//         tasks = JSON.parse(savedTasks);
+//         displayTasks();
+//     }
+// };
+
 //function to track own IP address on initial page load
 document.addEventListener("DOMContentLoaded", async () => {
   if (initialized) return;
   initialized = true;
   const res = await fetch("https://ipinfo.io/json");
   const data = await res.json();
-  console.log("data::::",data)
-  fetchData(data.ip)
+  fetchData(data.ip);
 });
 
 function validateSearchInput() {
@@ -55,10 +62,11 @@ async function fetchData(ipAddress) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
+    localStorage.setItem("Fetched API Data", JSON.stringify(data)); 
     renderAPIData(data);
   } catch (error) {
     console.error("Fetch error:", error);
-  }
+  } 
 }
 
 function renderAPIData(data) {
@@ -66,27 +74,25 @@ function renderAPIData(data) {
   locationSpan.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
   timezoneSpan.textContent = `UTC ${data.location.timezone}`;
   ispSpan.textContent = data.isp;
-  showCity(locationSpan.textContent)
+  showCity(locationSpan.textContent);
 }
 
 // fetch("https://jsonplaceholder.typicode.com/posts/1")
 //   .then(response => response.json())
 //   .then(data => console.log("Fetched data:", data))
 //   .catch(error => console.error("Fetch error:", error));
-const map = L.map('map').setView([20, 0], 2);
+const map = L.map("map").setView([20, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 async function showCity(city) {
   const res = await fetch(
-    `https://nominatim.openstreetmap.org/search?format=json&q=${city}`
+    `https://nominatim.openstreetmap.org/search?format=json&q=${city}`,
   );
 
   const data = await res.json();
-
   const lat = data[0].lat;
   const lon = data[0].lon;
-
-  map.setView([lat, lon], 10);
+  map.setView([lat, lon], 12);
   L.marker([lat, lon]).addTo(map);
 }
