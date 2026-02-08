fb19d0c2e59a887c04b8655483b5ba54cfdbc058
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-08 08:50:59 -0500

    added function to track own IP address on initial page load

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -4,41 +4,51 @@ const from = document.getElementById("form");
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 const submit = document.getElementById("submit");
-const searchInputError = document.getElementById("searchInputError")
+const searchInputError = document.getElementById("searchInputError");
 
 const ipAddressSpan = document.getElementById("ip-address");
 const locationSpan = document.getElementById("location");
 const timezoneSpan = document.getElementById("timezone");
 const ispSpan = document.getElementById("isp");
 
-function validateSearchInput(){
-    const regex =
-    /^(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$/;
-    if(searchInput.validity.valueMissing){
-        searchInput.setCustomValidity("Input is required");
-     }else if(!regex.test(searchInput.value)){
-        searchInput.setCustomValidity("Enter Valid IP Address.");
-     }else{
-        searchInput.setCustomValidity("");
-     }
-     searchInputError.textContent = searchInput.validationMessage;
-     return searchInput.checkValidity();
+let initialized = false;
 
+document.addEventListener("DOMContentLoaded", async () => {
+  if (initialized) return;
+  initialized = true;
+  const res = await fetch("https://ipinfo.io/json");
+  const data = await res.json();
+  fetchData(data.ip)
+});
+
+function validateSearchInput() {
+  const regex =
+    /^(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$/;
+  if (searchInput.validity.valueMissing) {
+    searchInput.setCustomValidity("Input is required");
+  } else if (!regex.test(searchInput.value)) {
+    searchInput.setCustomValidity("Enter Valid IP Address.");
+  } else {
+    searchInput.setCustomValidity("");
+  }
+  searchInputError.textContent = searchInput.validationMessage;
+  return searchInput.checkValidity();
 }
 
-searchForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    const  validate = validateSearchInput();
-    if(!validate){
-        return;
-    }
-    fetchData(searchInput.value);
-
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const validate = validateSearchInput();
+  if (!validate) {
+    return;
+  }
+  fetchData(searchInput.value);
 });
 
 async function fetchData(ipAddress) {
   try {
-    const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey= ${key} & ipAddress= ${ipAddress} `);
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey= ${key} & ipAddress= ${ipAddress} `,
+    );
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -49,15 +59,13 @@ async function fetchData(ipAddress) {
   }
 }
 
-
 function renderAPIData(data) {
   ipAddressSpan.textContent = data.ip;
   locationSpan.textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
+  
   timezoneSpan.textContent = `UTC ${data.location.timezone}`;
   ispSpan.textContent = data.isp;
 }
-
-
 
 // fetch("https://jsonplaceholder.typicode.com/posts/1")
 //   .then(response => response.json())
